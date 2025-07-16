30cd097810faa6648c7de021605f4e8301294185
NguyenVanBin_2151012001 <bin2003mail@gmail.com>
2025-07-16 15:03:12 +0700

    181/838 pdf

---
+++ htdocs/formprocess1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Say My Name</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Hello " . $_POST['name'] . "</h1>";
+    ?>
+    <pre>
+        <strong>DEBUG:</strong>
+        <?php
+            print_r ($_POST);
+        ?>
+    </pre>
+</body>
+</html>
